440ec74a25e36ef5bc4c646d6b03d0cb4a01e02c
Mo Mahboobian <momahboobian@gmail.com>
2023-10-20 00:45:26 +0100

    Hadeling player ships drag and drop

--- App.js
+++ App.js
@@ -56,54 +56,133 @@ const ship5 = new Ship("ship-5", 2);
 
 const ships = [ship1, ship2, ship3, ship4, ship5];
 // console.log(ships);
+let notDropped;
 
 // Randomly place each ship on the game board while avoiding overlapping or splitting
-function addShips(ship) {
-  const allBoardBlocks = document.querySelectorAll("#computer div");
+function addShips(user, ship, id) {
+  const allBoardBlocks = document.querySelectorAll(`#${user} div`);
   let randomBool = Math.random() < 0.5;
-  let isHorizontal = randomBool;
+  let isHorizontal = user === "player" ? rotate === 0 : randomBool;
 
   let randomStartIndex;
 
-  if (isHorizontal) {
-    // Ensure that the ship can be placed horizontally without wrapping
-    randomStartIndex = Math.floor(Math.random() * borderSize * borderSize);
-    while ((randomStartIndex % borderSize) + ship.length > borderSize) {
-      randomStartIndex = Math.floor(Math.random() * borderSize * borderSize);
-    }
+  if (id) {
+    randomStartIndex = id; // If an id is provided, use it as the starting index
   } else {
-    // Ensure that the ship can be placed vertically without going out of bounds
-    randomStartIndex = Math.floor(Math.random() * borderSize * borderSize);
-    while (
-      Math.floor(randomStartIndex / borderSize) + ship.length >
-      borderSize
-    ) {
-      randomStartIndex = Math.floor(Math.random() * borderSize * borderSize);
+    if (isHorizontal) {
+      // Ensure that the ship can be placed horizontally without wrapping
+      const maxStartIndex = borderSize * borderSize - ship.length;
+      randomStartIndex = Math.floor(Math.random() * (maxStartIndex + 1));
+    } else {
+      // For vertical ship
+      const maxStartIndex = borderSize * borderSize - ship.length * borderSize;
+      randomStartIndex = Math.floor(Math.random() * (maxStartIndex + 1));
     }
   }
 
   let shipBlocks = [];
+  let invalidPlacement = false;
 
   for (let i = 0; i < ship.length; i++) {
+    let blockIndex;
+
     if (isHorizontal) {
-      shipBlocks.push(allBoardBlocks[randomStartIndex + i]);
+      blockIndex = randomStartIndex + i;
     } else {
-      shipBlocks.push(allBoardBlocks[randomStartIndex + i * borderSize]);
+      blockIndex = randomStartIndex + i * borderSize;
+    }
+
+    const block = allBoardBlocks[blockIndex];
+
+    if (block && !block.classList.contains("taken")) {
+      shipBlocks.push(block);
+    } else {
+      invalidPlacement = true;
+      break;
     }
   }
 
-  const notTaken = shipBlocks.every(
-    (shipBlock) => !shipBlock.classList.contains("taken")
-  );
-
-  if (notTaken) {
+  if (!invalidPlacement) {
     shipBlocks.forEach((shipBlock) => {
       shipBlock.classList.add(ship.name);
       shipBlock.classList.add("taken");
     });
   } else {
-    addShips(ship);
+    if (user === "computer") addShips("computer", ship);
+    if (user === "player") notDropped = true;
   }
 }
 
-ships.forEach((ship) => addShips(ship));
+ships.forEach((ship) => addShips("computer", ship));
+
+// Handling player ships <<<<-----------------------------------
+const optionShips = document.querySelectorAll(".ships-container > div");
+
+optionShips.forEach((ship) => {
+  ship.addEventListener("dragstart", dragStart);
+  ship.addEventListener("dragend", dragEnd);
+});
+
+let draggedShip = null;
+
+function dragStart(event) {
+  // Store the ship being dragged
+  draggedShip = event.target;
+
+  // Add a class to indicate dragging
+  draggedShip.classList.add("dragging");
+}
+
+function dragEnd() {
+  // Remove the dragging class
+  if (draggedShip) {
+    draggedShip.classList.remove("dragging");
+  }
+}
+
+function allowDrop(e) {
+  e.preventDefault();
+}
+
+function dragOver(e) {
+  e.preventDefault();
+}
+
+function dragEnter(e) {
+  e.target.classList.add("hovered");
+}
+
+function dragLeave(e) {
+  e.target.classList.remove("hovered");
+}
+
+function drop(e) {
+  // Remove the hovered class
+  e.target.classList.remove("hovered");
+
+  // Get the ship's data-id attribute
+  const shipId = draggedShip.getAttribute("data-id");
+
+  // Place the ship on the game board
+  addShips("player", ships[shipId], parseInt(event.target.id));
+
+  // Remove the ship from the ship container
+  if (draggedShip) {
+    shipsContainer.removeChild(draggedShip);
+    draggedShip = null;
+  }
+}
+
+// Add event listeners to game board blocks and ships
+const blocksAndShips = document.querySelectorAll(
+  ".game-board .block, .ships-container > div"
+);
+
+blocksAndShips.forEach(function (element) {
+  element.addEventListener("dragstart", dragStart);
+  element.addEventListener("dragend", dragEnd);
+  element.addEventListener("dragover", dragOver);
+  element.addEventListener("dragenter", dragEnter);
+  element.addEventListener("dragleave", dragLeave);
+  element.addEventListener("drop", drop);
+});
