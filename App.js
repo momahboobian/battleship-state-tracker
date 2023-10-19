c8a3a5f01ac3c4df265f7c8243ad4e8a1cc99978
Mo Mahboobian <momahboobian@gmail.com>
2023-10-19 18:57:23 +0100

    creating board with differernt colours

--- App.js
+++ App.js
@@ -1,11 +1,40 @@
-// Storing the IDs
+// Select and storing the IDs & Classes
 const shipsContainer = document.querySelector(".ships-container");
 const shipDirection = document.querySelector("#direction-btn");
+const gameBoardsContainer = document.querySelector("#board-container");
 
-// Creating function for direction of the ship (Horizontal / Vertical)
+// Creating options for direction of the ships (Horizontal / Vertical)
 function direction() {
   const shipsArray = Array.from(shipsContainer.children);
-  shipsArray.forEach((ship) => (ship.style.transform = "rotate(90deg"));
+
+  // Check the rotation
+  rotate = 0;
+  rotate === 0 ? (rotate = 90) : (rotate = 0);
+
+  shipsArray.forEach((ship) => (ship.style.transform = `rotate(${rotate}deg)`));
 }
+
+// Creating Game Boards (Initializing "width" variable for scalability)
+const size = 10; // width x size & height x size
+
+function createBoard(colour, user) {
+  const gameBoards = document.createElement("div");
+  gameBoards.classList.add("game-board");
+  gameBoards.style.background = colour;
+  gameBoards.id = user;
+
+  for (let i = 0; i < size * size; i++) {
+    const blocks = document.createElement("div");
+    blocks.classList.add("block");
+    blocks.id = i;
+
+    gameBoards.append(blocks);
+  }
+
+  gameBoardsContainer.append(gameBoards);
+}
+
+createBoard("beige", "player");
+createBoard("bisque", "computer");
 
 shipDirection.addEventListener("click", direction);
