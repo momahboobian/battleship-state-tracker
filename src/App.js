16f5d8e0e0318c673e193d93fbcc90846289fc37
Mo Mahboobian <momahboobian@gmail.com>
2023-11-28 16:34:28 +0000

    Implementing the landing page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import "./App.css";
-import GameBoard from "./components/GameBoard";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">This is Battleship State Tracker</header>
-      <GameBoard />
+    <div className="d-flex justify-content-center align-items-center position-relative vh-100 text-white">
+      <div className="main-bg">{/* Main background */}</div>
+
+      <div className="text-center pt-5">
+        <h1 className="display-5 fw-lighter">Welcome to</h1>
+        <p className="battleship-text">Battleship</p>
+        <button className="btn-custom">Start</button>
+      </div>
     </div>
   );
 }
