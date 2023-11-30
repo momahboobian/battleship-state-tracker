c24a5f366f3c1665eee5b24f2dba5e742aec52f2
Mo Mahboobian <momahboobian@gmail.com>
2023-11-30 00:36:50 +0000

    stylizing the Signup Component

--- src/components/signUp/SignUp.js
+++ src/components/signUp/SignUp.js
@@ -2,17 +2,21 @@ import React from "react";
 
 function SignUp() {
   return (
-    <div>
+    <div className="text-center p-5">
       <h2 className="display-5 text-light fw-lighter">
         Sign up for the battle
       </h2>
-      <div className="mt-4">
-        <input
-          type="text"
-          className="form-control mb-3 custom-input"
-          placeholder="Your name"
-        />
-        <button className="btn btn-primary">Next</button>
+      <div className="d-flex flex-column align-items-center gap-4 p-4 pt-5">
+        <div className="col-4 input-effect">
+          <input className="effect-16" type="text" placeholder="" />
+          <label>First Name</label>
+          <span className="focus-border"></span>
+        </div>
+        <div className="d-flex gap-5 p-4">
+          <button className="btn-next">Back</button>
+
+          <button className="btn-next">Next</button>
+        </div>
       </div>
     </div>
   );
